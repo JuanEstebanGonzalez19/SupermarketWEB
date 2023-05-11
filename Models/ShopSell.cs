1c000cde7a1abbdf324d250eae3cbb9b80c9fdda
JuanEstebanGonzalez19 <juanestebangonzalezsierra0327@gmail.com>
2023-05-11 12:03:38 -0500

    Index Compra y Venta

---
+++ Models/ShopSell.cs
@@ -0,0 +1,12 @@
+﻿namespace SupermarketWEB.Models
+{
+    public class ShopSell
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Decimal Price { get; set; }
+        public int Sale_Percentage { get; set; }
+        public string? Description { get; set; }
+        public ICollection<Product>? Products { get; set; } = default!;
+    }
+}
---
+++ Pages/ShopSells/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.ShopSells
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public IndexModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ShopSell> ShopSells { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.ShopSells != null)
+            {
+                ShopSells = await _context.ShopSells.ToListAsync();
+            }
+        }
+    }
+}
