7d1670bed3bc118fb9a2c5279f0c76e09475de31
JuanEstebanGonzalez19 <juanestebangonzalezsierra0327@gmail.com>
2023-05-12 18:11:27 -0500

    Diseño

--- Pages/ShopSells/Index.cshtml.cs
+++ Pages/ShopSells/Index.cshtml.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SupermarketWEB.Data;
 using SupermarketWEB.Models;
+using System.Linq;
 
 namespace SupermarketWEB.Pages.ShopSells
 {
@@ -17,8 +19,10 @@ namespace SupermarketWEB.Pages.ShopSells
 
         public IList<ShopSell> ShopSells { get; set; } = default!;
 
+
         public async Task OnGetAsync()
         {
+
             if (_context.ShopSells != null)
             {
                 ShopSells = await _context.ShopSells.ToListAsync();
